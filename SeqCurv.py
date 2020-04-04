348b63904f9a2f7a3fd26551be183547372a5eb3
Théo L <theo.leleu3@etu.univ-lorraine.fr>
2020-04-04 19:55:55 +0200

    Update SeqCurv.py

--- SeqCurv.py
+++ SeqCurv.py
@@ -1,25 +1,17 @@
 import random
 import torch.contrib
 import torch
-from torchvision import datasets#gestion des données
 from torch.nn import functional as F#fonctions (gradient, cross entropy...)
 from torch import nn#neural netwaork
 import numpy as np#opérations matricielles
-from copy import deepcopy#copie
 import matplotlib.pyplot as plt#graphiques
 from tqdm import tqdm
 #from torch.contrib import SWA
 import torch.autograd as autograd
-import matplotlib.pyplot as plt#graphiques
-import matplotlib.image as mpimg
-from IPython import display
 import time
 import sys
 from typing import Dict
-from argparse import Namespace
-from torch import Tensor
 from torch.distributions import Categorical
-from torch.nn import Module
 
 epochs = 50
 lr = 1e-3
@@ -28,34 +20,23 @@ sample_size = 100
 hidden_size = 50
 num_task = 3
 
-
 assert int(torch.__version__.split(".")[1]) >= 4, "PyTorch 0.4+ required"
 
-def fim_diag(model: Module, data_loader: list, samples_no: int = None, empirical: bool = False, device: torch.device = None, verbose: bool = False, every_n: int = None) -> Dict[int, Dict[str, Tensor]]:
+def fim_diag(model: nn.Module, data_loader: list, samples_no: int = None) -> Dict[int, Dict[str, torch.Tensor]]:
     fim = {}
     for name, param in model.named_parameters():
         if param.requires_grad:
             fim[name] = torch.zeros_like(param)
 
     seen_no = 0
-    last = 0
-    tic = time.time()
     all_fims = dict({})
     i=0
     n=len(data_loader)
     while (samples_no is None or seen_no < samples_no) and i<n:
         data, target = data_loader[i]
         i+=1
-        if device is not None:
-            data = data.to(device)
-            if empirical:
-                target = target.to(device)
-
         logits = model(data)
-        if empirical:
-            outdx = target.unsqueeze(1)
-        else:
-            outdx = Categorical(logits=logits).sample().unsqueeze(1).detach()
+        outdx = Categorical(logits=logits).sample().unsqueeze(1).detach()
         samples = logits.gather(1, outdx)
 
         idx, batch_size = 0, data.size(0)
@@ -68,22 +49,6 @@ def fim_diag(model: Module, data_loader: list, samples_no: int = None, empirical
                     fim[name].detach_()
             seen_no += 1
             idx += 1
-
-            if verbose and seen_no % 100 == 0:
-                toc = time.time()
-                fps = float(seen_no - last) / (toc - tic)
-                tic, last = toc, seen_no
-                sys.stdout.write(f"\rSamples: {seen_no:5d}. Fps: {fps:2.4f} samples/s.")
-
-            if every_n and seen_no % every_n == 0:
-                all_fims[seen_no] = {n: f.clone().div_(seen_no).detach_()
-                                     for (n, f) in fim.items()}
-
-    if verbose:
-        if seen_no > last:
-            toc = time.time()
-            fps = float(seen_no - last) / (toc - tic)
-        sys.stdout.write(f"\rSamples: {seen_no:5d}. Fps: {fps:2.5f} samples/s.\n")
 
     for name, grad2 in fim.items():
         grad2 /= float(seen_no)
@@ -108,28 +73,27 @@ class MLP(nn.Module):
 
 def penalty(model,data,task):
     nbtasks=len(model)
-    pen=0
+    u={}
+    v={}
     means=[]
     for i in range(nbtasks):
       means.append({})
       for n, p in model[i].named_parameters():
         means[i][n] =p.data
     for i in range(nbtasks):
-      fim=fim_diag(model[task],data[i])
+      fim=fim_diag(model[i],data[i])
       fim=fim[400]
-      for n, p in model[task].named_parameters():
-        if 'weight' in n: 
-          dm=means[task][n]-means[i][n]
-          if len(dm.size())>1:
-            d=dm[:,0]
-          fimd=fim[n]
-          while len(fimd.size())>1:
-            fimd=fimd[0]
-          fimd=fimd[0]
-
-        
-          pen+= fimd*torch.dot(d,d)
-    return pen
+      d=means[i]
+      
+      for n, p in fim.items():
+          if 'weight' in n:
+            if n in u:
+              u[n]+=fim[n]
+              v[n]+=fim[n]*d[n]
+            else:
+              u[n]=fim[n]
+              v[n]=fim[n]*d[n]
+    return u,v
 
 
 def process(epochs,  train_loader : list, dev_loader : list, test_loader : list, importance, use_cuda=True, weight=None):
@@ -163,14 +127,26 @@ def train(model, optimizer, task : int, train_load: list, dev_load: list, import
     model[task].train()
     epoch_loss, dev_epoch_loss = 0, 0
     if ini==0:
-        penal=penalty(model,train_load,task)
+        u,v=penalty(model,train_load,task)
     else:
-        penal=0
-    for inp,target in train_load[task]:
-        optimizer.zero_grad()
-        output = model[task](inp)
-        loss = F.cross_entropy(output, target) + importance * penal
-        epoch_loss += float(loss.item())
+        u,v=0,0
+       
+    penal=0
+    for n, p in model[task].named_parameters():
+          if ini==0 and 'weight' in n:
+            d=p.data
+            un=u[n]
+            vn=v[n]
+            if len(d.size())>1:
+              d=d[:,0]
+              un,vn=un[:,0],vn[:,0]
+            penal+=torch.dot(d,un*d)-2*torch.dot(d,vn)
+
+    #for inp,target in train_load[task]:
+    #    optimizer.zero_grad()
+    #    output = model[task](inp)
+    #    loss = F.cross_entropy(output, target) + importance * penal
+        
 
     for inp,target in dev_load:
         optimizer.zero_grad()
@@ -179,11 +155,25 @@ def train(model, optimizer, task : int, train_load: list, dev_load: list, import
         dev_epoch_loss += float(dloss.item())
 
     for inp,target in train_load[task]:
+
+        penal=0
+        for n, p in model[task].named_parameters():
+          if ini==0 and 'weight' in n:
+            d=p.data
+            un=u[n]
+            vn=v[n]
+            if len(d.size())>1:
+              d=d[:,0]
+              un,vn=un[:,0],vn[:,0]
+            penal+=torch.dot(d,un*d)-2*torch.dot(d,vn)
+
         optimizer.zero_grad()
         output = model[task](inp)
         loss = F.cross_entropy(output, target) + importance * penal
+        epoch_loss += float(loss.item())
         loss.backward()
         optimizer.step()
+
     return epoch_loss / float(len(train_load[task])), dev_epoch_loss / float(len(dev_load))
 
 def test(model: nn.Module, data_loader: list):
